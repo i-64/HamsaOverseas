6d182382ca1c88453a1afbcda440c19c9bec699f
Sanul Raskar <sanulraskar@gmail.com>
2020-08-12 21:46:35 +0530

    removed secret key from setting.py

--- server/hamsaoverseas/hamsaoverseas/settings.py
+++ server/hamsaoverseas/hamsaoverseas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_keys import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0qvaot_w!(x4qi!4xccj4)e&-17r2fbmfg_#f5kh0g)2d=2g*t'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
